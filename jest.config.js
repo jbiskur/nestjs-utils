11eec06f081fb5e4dd47baa8a8f7b658149adf5d
jbiskur <jbiskur@gmail.com>
2020-10-28 13:59:40 +0000

    updated action removed old projects from jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   projects: [
-    "<rootDir>/packages/simple-async-module",
     "<rootDir>/packages/nest-js-test-utilities",
-    "<rootDir>/packages/test-app",
-    "<rootDir>/packages/nestjs-test-utilities",
-    "<rootDir>/packages/nestjs-test-utilities2",
   ],
 };
